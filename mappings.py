ad49d70e421ea1f6d0786bd2999961595ea1f907
Milos Knezevic <ushtipak@gmail.com>
2020-04-07 17:36:27 +0200

    Extract actual data from page now :)

--- mappings.py
+++ mappings.py
@@ -1,2 +1,5 @@
 
 CLASS_PAGES = "Box_120drhm-o_O-centerJustify_1nezfbd-o_O-centerAlign_19zvu2s-o_O-displayflex_poyjc"
+CLASS_TITLE = "card-title"
+CLASS_UNIVERSITY = "partner-name"
+CLASS_LINKS = "browse-result-card"
